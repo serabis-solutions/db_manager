504a3c681662420536854962950d9c086f7261f9
Mark Ellis <m@rkellis.com>
2019-01-30 22:59:33 +0000

    wip config - needs a DbManager::Config struct with the expected options

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use clap::clap_app;
 use env_logger;
 use log::debug;
+use config;
+use lazy_static::lazy_static;
+use std::sync::RwLock;
+
+lazy_static! {
+    static ref CONFIG: config::Config = {
+        let mut config = config::Config::default();
+        config.merge(config::File::with_name("dbmanager")).expect("error reading config");
+
+        config
+    };
+}
 
 fn main() {
     env_logger::init();
 
-    let matches = clap_app!(db_manager =>
+    let app = clap_app!(dbmanager =>
         (version: "1.0")
         (author: "Mark Ellis <m@rkellis.com>")
         (about: "Manages database migrations")
@@ -40,53 +52,46 @@ fn main() {
                 (@arg target: <target> "target to run against")
             )
         )
-    )
-    .get_matches();
+    );
 
-    //read config
-    let config = "I am the config now";
-
-    match matches.subcommand() {
-        ("add", Some(sub_m)) => add(&config, sub_m.value_of("name").unwrap()),
+    match app.get_matches().subcommand() {
+        ("add", Some(sub_m)) => add(sub_m.value_of("name").unwrap()),
         ("deploy", Some(sub_m)) => deploy(
-            &config,
             sub_m.value_of("target").unwrap(),
             sub_m.values_of("name"),
         ),
         ("verify", Some(sub_m)) => verify(
-            &config,
             sub_m.value_of("target").unwrap(),
             sub_m.values_of("name"),
         ),
         ("revert", Some(sub_m)) => revert(
-            &config,
             sub_m.value_of("target").unwrap(),
             sub_m.values_of("name").unwrap(),
         ),
         ("list", Some(sub_m)) => match sub_m.subcommand() {
-            ("available", Some(_)) => list_available(&config),
-            ("deployed", Some(sub_m)) => list_deployed(&config, sub_m.value_of("target").unwrap()),
+            ("available", Some(_)) => list_available(),
+            ("deployed", Some(sub_m)) => list_deployed(sub_m.value_of("target").unwrap()),
             _ => panic!("no subcommand - shouldn't ever get here"),
         },
         _ => panic!("no subcommand - shouldn't ever get here"),
     }
 }
 
-fn add(config: &str, name: &str) {
+fn add(name: &str) {
     debug!("add: \"{}\"", name);
 }
-fn deploy<'a>(config: &str, target: &str, migrations: Option<impl Iterator<Item = &'a str> + std::fmt::Debug>) {
+fn deploy<'a>(target: &str, migrations: Option<impl Iterator<Item = &'a str> + std::fmt::Debug>) {
     debug!("deploy: \"{}\" [{:?}]", target, migrations);
 }
-fn verify<'a>(config: &str, target: &str, migrations: Option<impl Iterator<Item = &'a str> + std::fmt::Debug>) {
+fn verify<'a>(target: &str, migrations: Option<impl Iterator<Item = &'a str> + std::fmt::Debug>) {
     debug!("verify: \"{}\" [{:?}]", target, migrations);
 }
-fn revert<'a>(config: &str, target: &str, migrations: impl Iterator<Item = &'a str> + std::fmt::Debug) {
+fn revert<'a>(target: &str, migrations: impl Iterator<Item = &'a str> + std::fmt::Debug) {
     debug!("revert: \"{}\" [{:?}]", target, migrations);
 }
-fn list_available(config: &str) {
-    debug!("list_available");
+fn list_available(){
+    debug!("list_available {:?}", CONFIG.get_str("fpp"));
 }
-fn list_deployed(config: &str, target: &str) {
+fn list_deployed(target: &str) {
     debug!("list_deployed: \"{}\"", target);
 }
